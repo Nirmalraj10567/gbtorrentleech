8225c45c6576ee3f340edbcf9d0a509d35d04eb3
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-08-31 13:36:23 +0530

    Create cloneHelper.py

---
+++ tobrot/helper_funcs/cloneHelper.py
@@ -0,0 +1,5 @@
+#This is code to clone the gdrive link using the gclone, all credit goes to the developer who has developed the glclone
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) gautamajay52
+
